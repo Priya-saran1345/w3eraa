6b34cae9930326acf2dc7f5ab10ca06e8a0e752c
Priya <priya.saran.w3era@gmail.com>
2024-11-20 13:45:58 +0530

    font size  changes

--- src/components/Common-Banner/index.tsx
+++ src/components/Common-Banner/index.tsx
@@ -11,9 +11,9 @@ const index = ({title,description,image,btnlink,btntext,image_alt ,status='false
     <div className='mx-auto xl:w-[75%] px-6 flex justify-center '>
         <div className='flex justify-between items-center py-4 md:py-10 gap-7'>
             <div className={`lg:w-1/2 flex flex-col  gap-6`}>
-                <h1 className=' text-[28px] leading-[45px] font-bold lg:text-[38px] '>{title}</h1>
+                <h1 className='leading-[45px] font-bold '>{title}</h1>
                 <StyledWrapper>
-                <div className="  text-homegrey text-justify "  dangerouslySetInnerHTML={{ __html:description }} >
+                <div className="text-homegrey text-justify "  dangerouslySetInnerHTML={{ __html:description }} >
                                         </div>
                                         </StyledWrapper>
 
--- src/components/ServicePackagePage.tsx
+++ src/components/ServicePackagePage.tsx
@@ -129,7 +129,7 @@ const Service_Pkackages = () => {
                             {
                                 data?.aboutservice?.title || data?.aboutservice?.subtitle || data?.aboutservice?.description ? (
                                     <div className='bg-white mt-12 mb-8 xl:w-[75%] px-4 xl:px-0 mx-auto'>
-                                        <h2 className='text-homeblack text-[24px] lg:text-[32px] text-center font-bold '>
+                                        <h2 className='text-homeblack  text-center font-bold '>
                                             {data?.aboutservice?.title || ''}
                                         </h2>
                                         <div className='flex justify-between mt-6 items-start lg:gap-16'>
